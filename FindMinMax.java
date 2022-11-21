b21484890f226792fa3ed5e067d6dfd9025502e8
Mr-Raja12 <rk9576899082@gmail.com>
2022-11-21 18:19:40 +0530

    Added: Enter two numbers and do the following arithmetic Operations find max and min

--- FindMinMax.java
+++ FindMinMax.java
@@ -1,24 +1,40 @@
 package com.bl.javapracticeproblems;
-
 import java.util.Scanner;
-
 public class FindMinMax {
 	public static void main(String[] args) {
-		int a,b,c=1;
-		Scanner r=new Scanner(System.in);
-		System.out.println("Enter 1st number");
-		a=r.nextInt();
-		System.out.println("Enter 2st number");
-		b=r.nextInt();
-	   int s= a+b*c;
-	   int p= c+a/b;
-	   int t= a%b+c;
-	   int q= a*b+c;
-		
-      System.out.println("First operation "+ s);
-      System.out.println("Secount operation "+ p);
-      System.out.println("Third operation "+ t);
-      System.out.println("Forth operation "+ q);
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter 1st Number : ");
+			int a = sc.nextInt();
+			System.out.println("Enter 2nd Number : ");
+			int b = sc.nextInt();
+			System.out.println("Enter 3rd Number : ");
+			int c = sc.nextInt();
+	//performing operation wises
+			int operation1 = a + b * c;
+			int operation2 = c + a / b;
+			int operation3 = a % b + c;
+			int operation4 = a * b + c;
 
+			int max = Math.max(Math.max(operation1, operation2), Math.max(operation3, operation4));
+
+			if (operation1 == max) {
+				System.out.println("operation1 is maximum = " + operation1);
+			} else if (operation2 == max) {
+				System.out.println("operation2 is maximum = " + operation2);
+			} else if (operation3 == max) {
+				System.out.println("operation3 is maximum = " + operation3);
+			} else
+				System.out.println("operation4 is maximum = " + operation4);
+
+			int min = Math.min(Math.min(operation1, operation2), Math.min(operation3, operation4));
+
+			if (operation1 == min) {
+				System.out.println("operation1 is minimum = " + operation1);
+			} else if (operation2 == min) {
+				System.out.println("operation2 is minimum = " + operation2);
+			} else if (operation3 == min) {
+				System.out.println("operation3 is minimum = " + operation3);
+			} else
+				System.out.println("operation4 is minimum = " + operation4);
 	}
 }
