ccc2ca16364d97fe1b59d74b0d7d6e28f79161c4
Mr-Raja12 <rk9576899082@gmail.com>
2022-11-21 17:54:32 +0530

    Added: To check word of the number by using digit number

--- NumberToWord.java
+++ NumberToWord.java
@@ -1,25 +1,35 @@
 package com.bl.javapracticeproblems;
-
+import java.util.Scanner;
 public class NumberToWord {
-	static void numberToWords(char num[])  
-	{  
-	int len = num.length, x = 0;  
-	if (len == 0)   
-	{  
-	System.out.println("The string is empty.");  
-	return;  
-	}      
-	String[] onedigit = new String[] {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};     
-	if (len == 1)   
-	{  
-	System.out.println(onedigit[num[0]-'0']);  
-	return;  
-	}   
-	System.out.println(onedigit[num[x] - '0']);  
-	}  
-	public static void main(String args[])  
-	{    
-	numberToWords("10".toCharArray());  
-	numberToWords("0".toCharArray());   
-	}  
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter digit : ");
+	int number = sc.nextInt();
+	if (number == 0) {
+		System.out.println("Number is Zero");
+	} else if (number == 1) {
+		System.out.println("Number is One");
+	} else if (number == 2) {
+		System.out.println("Number is Two");
+	} else if (number == 3) {
+		System.out.println("Number is Three");
+	} else if (number == 4) {
+		System.out.println("Number is Four");
+	} else if (number == 5) {
+		System.out.println("Number is Five");
+	} else if (number == 6) {
+		System.out.println("Number is Six");
+	} else if (number == 7) {
+		System.out.println("Number is Seven");
+	} else if (number == 8) {
+		System.out.println("Number is Eight");
+	} else if (number == 9) {
+		System.out.println("Number is Nine");
+	} else if (number == 10) {
+		System.out.println("Number is Ten");
+	} else {
+		System.out.println("Please enter value Between 0 to 10");
+	}
+
+  }
 }
