426f1aa0808530fc87a1b16a9d02d61a40146aec
Mr-Raja12 <rk9576899082@gmail.com>
2022-11-21 17:49:18 +0530

    Added: to find month name by it's number

--- MonthNameByNumber.java
+++ MonthNameByNumber.java
@@ -2,94 +2,61 @@ package com.bl.javapracticeproblems;
 import java.util.Scanner;
 public class MonthNameByNumber {
 	public static void main(String s[]){
-
-	       Scanner sc=new Scanner(System.in);
-
-	       System.out.print("Enter month name (full name): ");
-
-	       String name=sc.next().toUpperCase();//take a input from User in upper case
-
-	       sc.close();
-
-	       switch(name){
-
-	           case "JANUARY":
-
-	           System.out.println("Month Number: 1");
-
-	           break;
-
-	           case "FEBRUARY":
-
-	           System.out.println("Month Number: 2");
-
-	           break;
-
-	           case "MARCH":
-
-	           System.out.println("Month Number: 3");
-
-	           break;
-
-	           case "APRIL":
-
-	           System.out.println("Month Number: 4");
-
-	           break;
-
-	           case "MAY":
-
-	           System.out.println("Month Number: 5");
-
-	           break;
-
-	           case "JUNE":
-
-	           System.out.println("Month Number: 6");
-
-	           break;
-
-	           case "JULY":
-
-	           System.out.println("Month Number: 7");
-
-	           break;
-
-	           case "AUGUST":
-
-	           System.out.println("Month Number: 8");
-
-	           break;
-
-	           case "SEPTEMBER":
-
-	           System.out.println("Month Number: 9");
-
-	           break;
-
-	           case "OCTOBER":
-
-	           System.out.println("Month Number: 10");
-
-	           break;
-
-	           case "NOVEMBER":
-
-	           System.out.println("Month Number: 11");
-
-	           break;
-
-	           case "DECEMBER":
-
-	           System.out.println("Month Number: 12");
-
-	           break;
-
-	           default: System.out.println("Invalid Data.");
-
-	       }
-
+	           Scanner sc = new Scanner(System.in);
+	           System.out.println("Please enter month you want to check: ");
+	           int month = sc.nextInt();
+	           switch (month) {
+	               case 1: {
+	                   System.out.println("Your month is January");
+	                   break;
+	               }
+	               case 2: {
+	                   System.out.println("Your month is February");
+	                   break;
+	               }
+	               case 3: {
+	                   System.out.println("Your month is March");
+	                   break;
+	               }
+	               case 4: {
+	                   System.out.println("Your month is April");
+	                   break;
+	               }
+	               case 5: {
+	                   System.out.println("Your month is May");
+	                   break;
+	               }
+	               case 6: {
+	                   System.out.println("Your month is June");
+	                   break;
+	               }
+	               case 7: {
+	                   System.out.println("Your month is July");
+	                   break;
+	               }
+	               case 8: {
+	                   System.out.println("Your month is August");
+	                   break;
+	               }
+	               case 9: {
+	                   System.out.println("Your month is September");
+	                   break;
+	               }
+	               case 10: {
+	                   System.out.println("Your month is October");
+	                   break;
+	               }
+	               case 11: {
+	                   System.out.println("Your month is November");
+	                   break;
+	               }
+	               case 12: {
+	                   System.out.println("Your month is December");
+	                   break;
+	               }
+	               default:
+	                   System.out.println("Please enter valid input");
+	           }
+	           System.out.println("Thank you");
 	   }
-	
 }
-
